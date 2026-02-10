393a054d5cbb765a7fa16c7a2d910bb0a671ab44
Peter Wielander <mittgfu@gmail.com>
2026-02-10 13:35:13 -0800

    Override
    Signed-off-by: Peter Wielander <mittgfu@gmail.com>

--- packages/world-vercel/src/utils.ts
+++ packages/world-vercel/src/utils.ts
@@ -28,7 +28,8 @@ import { version } from './version.js';
  *
  * Example: 'https://workflow-server-git-branch-name.vercel.sh'
  */
-const WORKFLOW_SERVER_URL_OVERRIDE = '';
+const WORKFLOW_SERVER_URL_OVERRIDE =
+  'https://workflow-server-23wv5pp8u.vercel.sh';
 
 export interface APIConfig {
   token?: string;
